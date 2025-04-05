cba288d777c90ed758978d831aa8a794ac3e3f7a
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-05 20:26:26 +0530

    Update install-browser.js

--- install-browser.js
+++ install-browser.js
@@ -1,9 +1,11 @@
-import download from '@puppeteer/browsers';
+const puppeteerBrowsers = require('@puppeteer/browsers');
 
-const browser = await download({
-  browser: 'chrome',
-  buildId: '1200648', // stable version
-  cacheDir: './.chromium'
-});
+(async () => {
+  const browser = await puppeteerBrowsers.download({
+    browser: 'chrome',
+    buildId: '1200648',
+    cacheDir: './.chromium',
+  });
 
-console.log(`✅ Chromium downloaded to ${browser.executablePath}`);
+  console.log(`✅ Chromium downloaded to ${browser.executablePath}`);
+})();
