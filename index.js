d2d616f8cd558fd77568387cd7ab9711843eeece
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-07 10:33:34 +0530

    Update and rename bot.js to index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+import { Client, LocalAuth } from 'whatsapp-web.js';
+import qrcode from 'qrcode-terminal';
+import puppeteer from 'puppeteer';
+import dotenv from 'dotenv';
+import { connectDB, saveSession, getSession } from './db.js';
+
+dotenv.config();
+await connectDB();
+
+let client;
+
+(async () => {
+  const sessionData = await getSession();
+
+  client = new Client({
+    puppeteer: {
+      headless: true,
+      executablePath: puppeteer.executablePath(),
+      args: ['--no-sandbox']
+    },
+    session: sessionData
+  });
+
+  client.on('qr', qr => {
+    console.log('📲 Scan this QR code:');
+    qrcode.generate(qr, { small: true });
+  });
+
+  client.on('authenticated', async (session) => {
+    console.log("🔐 Authenticated");
+    await saveSession(session);
+  });
+
+  client.on('auth_failure', msg => {
+    console.error('❌ Authentication failure', msg);
+  });
+
+  client.on('ready', () => {
+    console.log('✅ WhatsApp is ready!');
+  });
+
+  client.on('message', async msg => {
+    if (msg.body === '!ping') {
+      msg.reply('🏓 Pong!');
+    }
+  });
+
+  client.initialize();
+})();
