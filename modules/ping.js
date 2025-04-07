b5081b25d2be6b0de507f3fbf52c34a4abad3c4a
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-08 01:28:23 +0530

    Update ping.js

--- modules/ping.js
+++ modules/ping.js
@@ -3,7 +3,9 @@ export default {
   description: 'Replies with Pong',
 
   async execute(msg, args, client) {
+    console.log('🏓 Pong!');
     const chat = await msg.getChat();
     await chat.sendMessage('🏓 Pong!');
+    await msg.edit('🏓 Pong!');
   }
 };
