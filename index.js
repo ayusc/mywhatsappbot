1440abe3d020670eb797d56568400c36f5b5dfff
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-07 18:59:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Connect to MongoDB
-await mongoose.connect(process.env.MONGODB_URI, {
+await mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
