dc34c58fdbd86700d91eb18102dc510b3205128b
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-08 10:10:35 +0530

    Update ping.js

--- modules/ping.js
+++ modules/ping.js
@@ -4,14 +4,12 @@ export default {
 
   async execute(msg, args, client) {
     const start = Date.now(); // Start timestamp
-    const chat = await msg.getChat();
-
-    await msg.delete(true, true); // Optional: delete the trigger message
-
+    
     const end = Date.now(); // End timestamp
     const timeTaken = end - start; // Milliseconds
 
     //console.log(`Pong ! (${timeTaken} ms)`);
-    await chat.sendMessage(`Pong !\nResponse time: ${timeTaken} ms`);
+    await msg.reply(`Pong !\nResponse time: ${timeTaken} ms`);
+    await msg.delete(true, true);
   }
 };
