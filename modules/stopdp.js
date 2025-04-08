2101dfc29438ecd1dd70b5d8d5c80100ad85816b
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-08 18:11:47 +0530

    Update stopdp.js

--- modules/stopdp.js
+++ modules/stopdp.js
@@ -1,20 +1,16 @@
 import { autodpInterval } from './autodp.js';
 
-let interval = autodpInterval;
-
 export default {
   name: '.stopdp',
-  description: 'Stops the auto profile picture updater',
+  description: 'Stop AutoDP',
 
   async execute(msg, args, client) {
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
+    if (autodpInterval) {
+      clearInterval(autodpInterval);
+      global.autodpInterval = null;
       await msg.reply('🛑 AutoDP stopped.');
     } else {
       await msg.reply('ℹ️ AutoDP is not running.');
     }
   }
 };
-
-export { interval as autodpInterval };
