7f8d2901a0ae3f0cdd82cbce692e92aca83fd738
Ayus Chatterjee <144832717+ayusc@users.noreply.github.com>
2025-04-08 10:30:33 +0530

    Update ping.js

--- modules/ping.js
+++ modules/ping.js
@@ -3,13 +3,15 @@ export default {
   description: 'Replies with Pong and response time',
 
   async execute(msg, args, client) {
-    const start = Date.now(); // Start timestamp
-    
-    const end = Date.now(); // End timestamp
-    const timeTaken = end - start; // Milliseconds
+    const start = Date.now();
 
-    //console.log(`Pong ! (${timeTaken} ms)`);
-    await msg.reply(`Pong !\nResponse time: ${timeTaken} ms`);
+    await msg.getChat(); // optional
+    await msg.reply('🏓 Pong!'); // main async operation
+    const end = Date.now();
+
+    const timeTaken = end - start;
+
+    await chat.sendMessage(`🏓 Pong!\nResponse time: ${timeTaken} ms`);
     await msg.delete(true, true);
   }
 };
